54969fb31c01cbf0d70fa196d285eb136049e64d
Apple <apple@iMac-Romaric.local>
2017-03-24 16:48:20 +0100

    Avancées test

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,25 +1,30 @@
 import PerfectLib
 import PerfectHTTP
 import PerfectHTTPServer
+import PerfectMustache
 
 // Create HTTP server.
 let server = HTTPServer()
 
+// Set the webroot directory so static files such as the logo, can be served
+server.documentRoot = "./webroot"
+
 // Register your own routes and handlers
 var routes = Routes()
+
 routes.add(method: .get, uri: "/", handler: {
     request, response in
     response.setHeader(.contentType, value: "text/html")
     response.appendBody(string: "<html><title>Hello, world!</title><body>Hello, world!</body></html>")
     response.completed()
-}
+    }
 )
 
 // Add the routes to the server.
 server.addRoutes(routes)
 
-// Set a listen port of 8181
-server.serverPort = 8181
+// Set a listen port of 8080
+server.serverPort = 8080
 
 do {
     // Launch the HTTP server.
